2199c6c7d5f26d6c53db5f966886c24b0c8c3fd5
cosmobobak <cosmobobak@gmail.com>
2021-12-26 22:54:53 +0000

    refmt

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,46 @@
-#![allow(unused_imports, dead_code)]
+use crate::{
+    coverttt::CoverTTT,
+    game::Game,
+    solver::{eval_to_string, solve},
+    tictactoe::TicTacToe,
+};
 
-use coverttt::CoverTTT;
-use crate::{perft::perft, tictactoe::TicTacToe, game::Game, solver::{INF, solve, eval_to_string}, coverttt::{CoverTTTMove, Size}};
-
-
-mod game;
-mod solver;
-mod perft;
 mod coverttt;
+mod game;
+mod perft;
+mod solver;
 mod tictactoe;
 
-fn main() {
-    // println!("Solving Noughts and Crosses");
-    // solver::print_solve_info(TicTacToe::new());
-    // println!();
-    // println!("Solving Cover Tic-Tac-Toe");
-    // solver::print_solve_info(CoverTTT::new());
-
-    // println!("CTTT opening move evaluations (sorted worst-to-best):");
-    // let mut root = CoverTTT::new();
-    // let mut moves = Vec::with_capacity(27);
-    // root.generate_legal_moves(&mut moves);
-    // let mut evals = moves.into_iter().map(|m| {
-    //     root.push(m);
-    //     let value = solve(root);
-    //     root.pop(m);
-    //     (m, value)
-    // }).collect::<Vec<_>>();
-    // evals.sort_by(|a, b| (1.0 / a.1 as f32).partial_cmp(&(1.0 / b.1 as f32)).unwrap());
-    // for (m, value) in evals {
-    //     println!("move {}: {}", m, eval_to_string(value + value.signum()));
-    // }
-
-    let mut game = CoverTTT::new();
-    // game.push(CoverTTTMove::new(4, Size::Big));
-    // game.push(CoverTTTMove::new(8, Size::Big));
-    // game.push(CoverTTTMove::new(2, Size::Big));
-    // game.push(CoverTTTMove::new(6, Size::Big));
-    // game.push(CoverTTTMove::new(7, Size::Big));
-    // game.push(CoverTTTMove::new(1, Size::Medium));
-    // game.push(CoverTTTMove::new(3, Size::Medium));
-    // game.push(CoverTTTMove::new(7, Size::Medium));
-    // game.push(CoverTTTMove::new(0, Size::Medium));
-    while !game.is_terminal() {
-        println!("{}", game);
-        game.push(solver::best_move(game));
+fn invert(val: i32) -> f64 {
+    if val == 0 {
+        0.0
+    } else {
+        1.0 / val as f64
     }
-    println!("{}", game);
+}
+
+fn main() {
+    println!("Solving Noughts and Crosses");
+    solver::print_solve_info(TicTacToe::new());
+    println!();
+    println!("Solving Cover Tic-Tac-Toe");
+    solver::print_solve_info(CoverTTT::new());
+
+    println!("CTTT opening move evaluations (sorted worst-to-best):");
+    let mut root = CoverTTT::new();
+    let mut moves = Vec::with_capacity(27);
+    root.generate_legal_moves(&mut moves);
+    let mut evals = moves
+        .into_iter()
+        .map(|m| {
+            root.push(m);
+            let value = solve(root);
+            root.pop(m);
+            (m, value)
+        })
+        .collect::<Vec<_>>();
+    evals.sort_by(|a, b| invert(a.1).partial_cmp(&invert(b.1)).unwrap());
+    for (m, value) in evals {
+        println!("move {}: {}", m, eval_to_string(value + value.signum()));
+    }
 }
